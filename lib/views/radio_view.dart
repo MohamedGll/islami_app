1486bf11a66f3eaa16ef7969109089cae09000e0
khorshed <mg6914949@gmail.com>
2024-07-25 03:41:26 +0300

    create radio view

--- lib/views/radio_view.dart
+++ lib/views/radio_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/constant.dart';
 
 class RadioView extends StatelessWidget {
   const RadioView({super.key});
@@ -6,6 +7,55 @@ class RadioView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Image(
+          image: AssetImage(
+            'assets/images/radio_image.png',
+          ),
+          height: 222,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 28),
+          child: Text(
+            'إذاعة القراّن الكريم',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.skip_previous,
+                size: 42,
+                color: kPrimaryColorLight,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.play_arrow_rounded,
+                  size: 62,
+                  color: kPrimaryColorLight,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.skip_next,
+                size: 42,
+                color: kPrimaryColorLight,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
